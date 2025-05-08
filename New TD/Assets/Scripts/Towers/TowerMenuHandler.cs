df319c69b96b3bed28c9bb981422cde4ddd7dab8
Oleksandr <karpachev90@gmail.com>
2025-05-08 17:53:46 +0200

    Towers almost ready

---
+++ New TD/Assets/Scripts/Towers/TowerMenuHandler.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class TowerMenuHandler : MonoBehaviour
+{
+    [SerializeField] private GameObject menuUI;
+
+    private void OnMouseDown()
+    {
+        if (menuUI != null)
+        {
+            menuUI.SetActive(true);
+            GameManager.Instance.PauseGame();
+        }
+    }
+
+    public void CloseMenu()
+    {
+        if (menuUI != null)
+        {
+            menuUI.SetActive(false);
+            GameManager.Instance.ResumeGame();
+        }
+    }
+
+}
